873bec7e687b5961a30819dbaf0c911fc47d4469
ad1jadhav <138484744+ad1jadhav@users.noreply.github.com>
2023-10-25 18:11:22 +0530

    Add files via upload

---
+++ Telegram Bot/bot.py
@@ -0,0 +1,19 @@
+import telebot 
+
+Token = "6654688487:AAEAM35aiK7-whHimgijyW6663I03_U7fO8"
+
+bot = telebot.TeleBot(Token) 
+
+@bot.message_handler(['start'])
+def start(message):
+  bot.reply_to(message,"Welcome to Upmarket. This bot is built by Aditya Jadhav.")
+  
+@bot.message_handler(['about'])
+def start(message):
+  bot.reply_to(message,"Upmarket is a fintech startup that provides roboadvisory services to early-stage stock traders in India.")  
+  
+@bot.message_handler(['help'])
+def start(message):
+  bot.reply_to(message,"Upmarket empowers traders with AI-driven insights and personalized strategies to enhance their stock marcket success.")    
+
+bot.polling()
